b7f95cafa10d69dce455d5fcb192ad4be2b0879b
josealdunate <aldunate.jose91@gmail.com>
2021-03-27 19:28:09 -0300

    made webpage responsive; created script.js to hide and show navbar on small devices

---
+++ script.js
@@ -0,0 +1,22 @@
+const navLinks = document.getElementById("navLinks");
+const nav = document.getElementById("navbar");
+
+
+function toggleNav() {
+    if (navLinks.style.display === "none") {
+        navLinks.style.display = "block";
+        nav.style.position = "fixed";
+    } else {
+        navLinks.style.display = "none";
+        nav.style.position = "sticky";
+    }
+}
+
+function hideNav() {
+    if (document.documentElement.clientWidth <= 550) {
+        toggleNav();
+    }
+}
+
+
+
